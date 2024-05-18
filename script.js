e1ca1c635b366bcd54b951ab266ba44d574d2e18
Samuel Mburu <sammxuser@gmail.com>
2024-05-18 14:09:53 +0300

    Final project

---
+++ script.js
@@ -0,0 +1,15 @@
+let createButton = document.querySelector('button');
+let errorParagraph = document.querySelector('.error');
+
+createButton.addEventListener('click', () => {
+  let password = document.getElementById('password1').value;
+  let password2 = document.getElementById('password2').value;
+
+  if (password !== password2) {
+    errorParagraph.textContent = '*Passwords do not match*';
+
+    // password.style.borderColor = 'red';
+    // password2.style.borderColor = 'red';
+    alert(password.style.borderColor);
+  }
+});
